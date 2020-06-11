275817e5fcc1006093dc0dc34526a54d097f0028
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-06-11 13:07:35 +0300

    итог

--- main.js
+++ main.js
@@ -1,77 +1,43 @@
-function randomInt(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-let isNumber = function (n){
+let isNum = function (n){
     return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let count = 10;
-let number;
+const game = function(){
+    const randomNum = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+    let count = 10;
 
-function guess(str){
-    userNum = prompt(str);
-    if (!isNumber(userNum)){
-        if (userNum == null){
-            return null;
-        } else {
-            alert('ВВОДИТЕ ЧИСЛО!');
-            userNum = prompt(str);
-        }
-    }
-}
-
-
-let game = function(){
-    count--;
-    function adjustment(n){
-        if (count > 0){
-            if (n == null){
-                return null;
-            } else if (n > number){
-                guess('меньше! (осталось ' + count + ' попток)');
-                return false;
-            } else if (n < number){
-                guess('больше! (осталось ' + count + ' попток)');
-                return false;
-            } else if (n == number){
-                return true;
+    return function repeatPlay() {
+        let num = prompt('Угадайте число от 1 до 100!');
+        if (isNum(num)) {
+            let userNumber = +num;
+            count --;
+            if (count >= 0) {
+                if (userNumber < randomNum) {
+                    alert('Загаданное число больше, осталось попыток: ' + count);
+                    return repeatPlay();
+                } else if (userNumber > randomNum) {
+                    alert('Загаданное число меньше, осталось попыток: ' + count);
+                    return repeatPlay();
+                } else {
+                    if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+                        game()();
+                    } else {
+                        alert('Вы завершили игру');
+                    }
+                }
+            } else {
+                if (confirm('Попытки закончились, хотите сыграть еще?')) {
+                    game()();
+                } else {
+                    alert('Вы завершили игру');
+                }
             }
+        } else if (num === null) {
+            alert('Вы завершили игру');
         } else {
-            return '-1';
+            return repeatPlay();
         }
-    }
-    let res = adjustment(userNum);
-    count--;
-    if (res === '-1'){
-        if (!confirm('Ваши попытки закончились. Хотите сыграть еще?')){
-            return;
-        } else {
-            window.location.reload();
-            return;
-        }
-    }
-    else if (res == null){
-        return alert ('Вы отменили игру');
-    }
-    else if (!res){
-        let res = adjustment(userNum);
-        game();
-    } else{
-        if (!confirm('Вы выиграли!!! Хотите сыграть еще?')){
-            return;
-        } else {
-            window.location.reload();
-            return;
-        }
-    }
+    };
 };
-
-const startGame = function (){
-    number = randomInt(1, 100);
-    console.log(number);
-    guess('Угадай число от 1 до 100');
-
-    let userNum;
-    game();
-};
+const startGame = game();
 startGame();
